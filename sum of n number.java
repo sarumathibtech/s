052827bfccf25a9041b27b70c37572b59c6e6d1e
sarumathibtech <sarumathimuthusamy@gmail.com>
2016-07-13 19:12:01 -0700

    Create sum of n number.java

---
+++ sum of n number.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+public class sum_of_number
+{
+	public static void main(String args[])
+	{
+		int n,i,sum=0;
+		Scanner ob=new Scanner(System.in);
+		System.out.println("Enter the N value");
+				n=ob.nextInt();
+				
+				for(i=1;i<=n;i++)
+					sum+=i;
+		System.out.println("Natural number "+n+" sum is "+sum);	
+	}
+}
