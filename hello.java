149745ad7d1e90923550e8dc38274e4390509228
sarumathibtech <sarumathimuthusamy@gmail.com>
2016-07-13 18:51:06 -0700

    Create hello.java

---
+++ hello.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+public class hello
+{
+	public static void main(String args[])
+	{
+		int n,i;
+		Scanner ob=new Scanner(System.in);
+		System.out.println("Enter the N value");
+				n=ob.nextInt();
+				
+				for(i=0;i<n;i++)
+					System.out.println("Hello");	
+	}
+}
